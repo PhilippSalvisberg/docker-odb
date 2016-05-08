31f8dfde7943c16b3fc59ef34dcbb656d19518bb
PhilippSalvisberg <philipp.salvisberg@trivadis.com>
2016-05-08 08:53:30 +0200

    using Data_Warehouse.dbc template in DBCA, use environment variables defined in Dockerfile, installs oddgen, upgrades APEX

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,33 +25,44 @@ case "$1" in
 			su oracle -c 'echo startup\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba'
 		else
 			echo "Database not initialized. Initializing database."
-
-			#printf "Setting up:\nprocesses=$processes\nsessions=$sessions\ntransactions=$transactions\n"
-
 			mv /u01/app/oracle-product/12.1.0/xe/dbs /u01/app/oracle/dbs
 			ln -s /u01/app/oracle/dbs /u01/app/oracle-product/12.1.0/xe/dbs
-
+			echo "Copying sample schemas"
+			rm -rf $ORACLE_HOME/demo/schema
+			$ORACLE_HOME/bin/unzip /tmp/db-sample-schemas-master.zip -d $ORACLE_HOME/demo/
+			mv $ORACLE_HOME/demo/db-sample-schemas-master $ORACLE_HOME/demo/schema
+			cd $ORACLE_HOME/demo/schema
+			perl -p -i.bak -e 's#__SUB__CWD__#'$(pwd)'#g' *.sql */*.sql */*.dat
+			chown oracle:dba $ORACLE_HOME/demo/schema
 			echo "Starting tnslsnr"
 			su oracle -c "/u01/app/oracle/product/12.1.0/xe/bin/tnslsnr &"
-			#create DB for SID: xe
-			su oracle -c "$ORACLE_HOME/bin/dbca -silent -createDatabase -templateName General_Purpose.dbc -gdbname xe.oracle.docker -sid xe -responseFile NO_VALUE -characterSet AL32UTF8 -totalMemory $DBCA_TOTAL_MEMORY -emConfiguration LOCAL -pdbAdminPassword oracle -sysPassword oracle -systemPassword oracle"
-			
+			#create DB for SID: xe (sample schemas HR, OC, PM, IX not installed, reason nyk)
+			su oracle -c "$ORACLE_HOME/bin/dbca -silent -createDatabase -templateName Data_Warehouse.dbc -gdbname xe.oracle.docker -sid xe -responseFile NO_VALUE -characterSet AL32UTF8 -totalMemory $DBCA_TOTAL_MEMORY -emConfiguration LOCAL -pdbAdminPassword oracle -sysPassword ${PASS} -systemPassword ${PASS}"
 			echo "Configuring Apex console"
 			cd $ORACLE_HOME/apex
-			su oracle -c 'echo -e "0Racle$\n8082" | $ORACLE_HOME/bin/sqlplus -S / as sysdba @apxconf > /dev/null'
+			su oracle -c 'echo -e "${APEX_PASS}\n${HTTP_PORT}" | $ORACLE_HOME/bin/sqlplus -S / as sysdba @apxconf > /dev/null'
 			su oracle -c 'echo -e "${ORACLE_HOME}\n\n" | $ORACLE_HOME/bin/sqlplus -S / as sysdba @apex_epg_config_core.sql > /dev/null'
 			su oracle -c 'echo -e "ALTER USER ANONYMOUS ACCOUNT UNLOCK;" | $ORACLE_HOME/bin/sqlplus -S / as sysdba > /dev/null'
-			echo "Database initialized. Please visit http://#containeer:8082/em http://#containeer:8082/apex for extra configuration if needed"
+			echo "Database initialized."
+			echo "Installing oddgen schemas."
+			. /install_oddgen.sh
+			if [ $WEB_CONSOLE == "true" ]; then
+				echo "Upgrading APEX installation to version ${APEX_VERSION}"
+				. /upgrade_apex.sh install
+			else 
+				echo "web management console and APEX is disabled, no need to upgrade APEX."
+			fi			
 		fi
 
 		if [ $WEB_CONSOLE == "true" ]; then
 			echo 'Starting web management console'
-			su oracle -c 'echo EXEC DBMS_XDB.sethttpport\(8082\)\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba'
+			su oracle -c 'echo EXEC DBMS_XDB.sethttpport\(${HTTP_PORT}\)\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba'
+			echo "Web management console initialized. Please visit http://#container:${HTTP_PORT}/em http://#container:${HTTP_PORT}/apex for extra configuration if needed"
 		else
 			echo 'Disabling web management console'
 			su oracle -c 'echo EXEC DBMS_XDB.sethttpport\(0\)\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba'
 		fi
-
+		
 		echo "Database ready to use. Enjoy! ;)"
 
 		##
