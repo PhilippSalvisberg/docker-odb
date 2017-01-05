eaf90019eaa34c22c393fd5c5b66fd60cabb1c97
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-01-05 11:31:38 +0100

    remove settings done in oracle12ee image

--- assets/setenv.sh
+++ assets/setenv.sh
@@ -3,25 +3,10 @@
 # ignore secure linux
 setenforce Permissive
 
-# environment variables (not configurable when creating a container)
-export ORACLE_HOME=/u01/app/oracle/product/12.1.0.2/dbhome
-export ORACLE_BASE=/u01/app/oracle
-export TERM=linux
-
 # Prevent owner issues on mounted folders
 chown -R oracle:dba /u01/app/oracle
 rm -f /u01/app/oracle/product
 ln -s /u01/app/oracle-product /u01/app/oracle/product
-
-# Add Oracle to path
-export PATH=${ORACLE_HOME}/bin:$PATH
-if grep -q "PATH" ~/.bashrc
-then
-	echo "Found PATH definition in ~/.bashrc"
-else
-	echo "Extending PATH in ~/.bashrc"
-	printf "\nPATH=${PATH}\n" >> ~/.bashrc
-fi
 
 # Create tnsnames.ora
 if [ -f "${ORACLE_HOME}/network/admin/tnsnames.ora" ]
