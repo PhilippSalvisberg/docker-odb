84a457702784fc3b036448c19f3b82ae9eb56265
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-04-02 04:18:52 +0200

    ensure current directory contains no soft links

--- assets/oracle12ee_setup.sh
+++ assets/oracle12ee_setup.sh
@@ -82,9 +82,9 @@ rm -r -f ${ORACLE_HOME}/demo/schema
 echo "extracting Oracle sample schemas..."
 unzip /tmp/db-sample-schemas-master.zip -d ${ORACLE_HOME}/demo/ > /dev/null
 mv ${ORACLE_HOME}/demo/db-sample-schemas-master ${ORACLE_HOME}/demo/schema
-cd ${ORACLE_HOME}/demo/schema
 # ensure ORACLE_HOME does not contain soft links to avoid "ORA-22288: file or LOB operation FILEOPEN failed"  (for Oracle sample schemas)
 ORACLE_HOME=`readlink -f ${ORACLE_HOME}`
+cd ${ORACLE_HOME}/demo/schema
 perl -p -i.bak -e 's#__SUB__CWD__#'$(pwd)'#g' *.sql */*.sql */*.dat > /dev/null
 # reset environment (ORACLE_HOME)
 . /.oracle_env
