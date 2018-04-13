36a8da41fc9e0aadbdbf5621f45c3f008dcdb3be
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2018-04-14 01:27:53 +0200

    add Dockerfile for 18.0 database

---
+++ OracleDatabase/18.0/assets/entrypoint.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+reuse_database(){
+	echo "Reuse existing database."
+	if grep -q "$ORACLE_SID:" /etc/oratab ; then
+		# starting an existing container
+		echo "Database already registred in /etc/oratab"
+	else
+		# new container with an existing volume
+		echo "Registering Database in /etc/oratab"
+		echo "$ORACLE_SID:$ORACLE_HOME:N" >> /etc/oratab
+	fi
+	chown oracle:dba /etc/oratab
+	chmod 664 /etc/oratab
+	rm -rf /u01/app/oracle-product/18.0.0/dbhome/dbs
+	ln -s /u01/app/oracle/dbs /u01/app/oracle-product/18.0.0/dbhome/dbs
+	gosu oracle bash -c "${ORACLE_HOME}/bin/lsnrctl start"
+	gosu oracle bash -c 'echo startup\; | ${ORACLE_HOME}/bin/sqlplus -s -l / as sysdba'
+}
+
+set_timezone(){
+	echo "Change timezone to Central European Time (CET)..."
+	unlink /etc/localtime
+	ln -s /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+}
+
+create_database(){
+	echo "Creating database."
+	mv /u01/app/oracle-product/18.0.0/dbhome/dbs /u01/app/oracle/dbs
+	ln -s /u01/app/oracle/dbs /u01/app/oracle-product/18.0.0/dbhome/dbs
+	gosu oracle bash -c "${ORACLE_HOME}/bin/lsnrctl start"
+	if [ $DBEXPRESS == "true" ]; then
+		EM_CONFIGURATION=DBEXPRESS
+	else
+		EM_CONFIGURATION=NONE
+	fi
+	if [ $MULTITENANT == "true" ]; then
+		gosu oracle bash -c "${ORACLE_HOME}/bin/dbca -silent -createDatabase -templateName General_Purpose.dbc \
+		   -gdbname ${GDBNAME} -sid ${ORACLE_SID} -createAsContainerDatabase true -numberOfPDBs 1 -pdbName ${PDB_NAME} \
+		   -responseFile NO_VALUE -characterSet AL32UTF8 -totalMemory ${DBCA_TOTAL_MEMORY} -emConfiguration ${EM_CONFIGURATION} \
+		   -sysPassword ${PASS} -systemPassword ${PASS} -pdbAdminUserName pdbadmin -pdbAdminPassword ${PASS} \
+		   -initparams _exadata_feature_on=TRUE"
+	else
+		gosu oracle bash -c "${ORACLE_HOME}/bin/dbca -silent -createDatabase -templateName General_Purpose.dbc \
+		   -gdbname ${SERVICE_NAME} -sid ${ORACLE_SID} -responseFile NO_VALUE -characterSet AL32UTF8 \
+		   -totalMemory $DBCA_TOTAL_MEMORY -emConfiguration ${EM_CONFIGURATION} -sysPassword ${PASS} -systemPassword ${PASS} \
+		   -initparams _exadata_feature_on=TRUE"
+	fi
+	echo "Configure listener."
+	gosu oracle bash -c 'echo -e "ALTER SYSTEM SET LOCAL_LISTENER='"'"'(ADDRESS = (PROTOCOL = TCP)(HOST = $(hostname))(PORT = 1521))'"'"' SCOPE=BOTH;\n ALTER SYSTEM REGISTER;\n EXIT" | ${ORACLE_HOME}/bin/sqlplus -s -l / as sysdba'
+	if [ $MULTITENANT == "true" ]; then
+		echo "Save open state of PDB."
+		gosu oracle bash -c 'echo -e "ALTER PLUGGABLE DATABASE opdb1 OPEN;\n ALTER PLUGGABLE DATABASE ${PDB_NAME} SAVE STATE;\n EXIT" | ${ORACLE_HOME}/bin/sqlplus -s -l / as sysdba'
+	fi;
+	echo "Applying data patches."
+	gosu oracle bash -c "cd ${ORACLE_HOME}/OPatch && (./datapatch -verbose)"
+	echo "Workaround for bug 25710407"
+	gosu oracle bash -c 'echo -e "EXEC dbms_stats.init_package();\n EXIT" | ${ORACLE_HOME}/bin/sqlplus -s -l / as sysdba'
+	echo "Setting TWO_TASK environment for default connection."
+	if [ $MULTITENANT == "true" ]; then
+		export CONNECT_STRING=${PDB_NAME}
+	else
+		export CONNECT_STRING=${ORACLE_SID}
+	fi
+	echo "export CONNECT_STRING=${CONNECT_STRING}" >> /.oracle_env
+	echo "export CONNECT_STRING=${CONNECT_STRING}" >> /home/oracle/.bash_profile
+	echo "export CONNECT_STRING=${CONNECT_STRING}" >> /root/.bashrc
+	if [ $APEX == "true" ]; then
+		. /assets/install_apex.sh
+	fi
+	if [ $DBEXPRESS == "true" -o  \( $APEX == "true" -a $ORDS == "false" -a $MULTITENANT == "false" \) ]; then
+		echo "Enabable XDB HTTP port for EM Database Express and EPG on non-CDB database."
+		if [ $MULTITENANT == "true" ]; then
+			gosu oracle bash -c 'echo EXEC DBMS_XDB_CONFIG.setglobalportenabled\(true\)\; | ${ORACLE_HOME}/bin/sqlplus -s -l / as sysdba'
+		fi
+		gosu oracle bash -c 'echo EXEC DBMS_XDB.sethttpport\(8080\)\; | ${ORACLE_HOME}/bin/sqlplus -s -l / as sysdba'
+	fi
+	if [ $ORDS == "true" ]; then
+		echo "Installing ORDS."
+		gosu oracle bash -c "/assets/install_ords.sh"
+	fi
+	echo "Installing schema SCOTT."
+	# setting TWO_TASK causes connections using O/S authentication to fail, e.g. "sqlplus / as sysdba".
+	TWO_TASK=${CONNECT_STRING}
+	${ORACLE_HOME}/bin/sqlplus sys/${PASS}@${TWO_TASK} as sysdba @${ORACLE_HOME}/rdbms/admin/utlsampl.sql
+	unset TWO_TASK
+	echo "Installing Oracle sample schemas."
+	. /assets/install_oracle_sample_schemas.sh
+	echo "Installing FTLDB."
+	. /assets/install_ftldb.sh
+	echo "Installing tePLSQL."
+	. /assets/install_teplsql.sh
+	echo "Installing oddgen examples/tutorials"
+	. /assets/install_oddgen.sh
+}
+
+start_database(){
+	# Startup database if oradata directory is found otherwise create a database
+	if [ -d ${ORACLE_BASE}/oradata ]; then
+		reuse_database
+	else
+		set_timezone
+		create_database
+	fi
+
+	# start ORDS
+	gosu oracle bash -c "/assets/start_ords.sh"
+
+	# Successful installation/startup
+	echo ""
+	echo "Database ready to use. Enjoy! ;-)"
+
+	# trap interrupt/terminate signal for graceful termination
+	trap "gosu oracle bash -c 'echo Starting graceful shutdown... && echo shutdown immediate\; | ${ORACLE_HOME}/bin/sqlplus -S / as sysdba && /assets/stop_ords.sh && ${ORACLE_HOME}/bin/lsnrctl stop'" INT TERM
+
+	# waiting for termination of tns listener
+	PID=`ps -e | grep tnslsnr | awk '{print $1}'`
+	while test -d /proc/$PID; do sleep 1; done
+	echo "Graceful shutdown completed."
+}
+
+# set environment
+. /assets/setenv.sh
+
+# Exit script on non-zero command exit status
+set -e
+
+case "$1" in
+	'')
+		# default behaviour when no parameters are passed to the container
+		start_database
+		;;
+	*)
+		# use parameters passed to the container
+		echo ""
+		echo "Overridden default behaviour. Run /assets/entrypoint.sh when ready."
+		$@
+		;;
+esac
---
+++ OracleDatabase/18.0/assets/install_ords.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+mkdirs(){
+    mkdir -p ${ORACLE_BASE}/ords/params
+    mkdir -p ${ORACLE_BASE}/ords/conf
+    mkdir -p ${ORACLE_BASE}/ords/logs
+}
+
+set_params(){
+	if [ $MULTITENANT == "true" ]; then
+		THE_SERVICE_NAME=$PDB_SERVICE_NAME
+	else
+		THE_SERVICE_NAME=$SERVICE_NAME
+	fi
+	cat >${ORACLE_BASE}/ords/params/ords_params.properties <<EOF
+config.dir=${ORACLE_BASE}/ords/conf
+db.hostname=${HOSTNAME}
+db.port=1521
+db.servicename=${THE_SERVICE_NAME}
+db.username=APEX_PUBLIC_USER
+db.password=${PASS}
+migrate.apex.rest=false
+plsql.gateway.add=true
+rest.services.apex.add=true
+rest.services.ords.add=true
+schema.tablespace.default=APEX
+schema.tablespace.temp=TEMP
+standalone.mode=false
+standalone.use.https=false
+standalone.http.port=8081
+standalone.access.log=${ORACLE_BASE}/ords/logs
+standalone.context.path=/ords
+standalone.doc.root=${ORACLE_HOME}/ords/docs/javadoc
+standalone.scheme.do.not.prompt=true
+standalone.static.context.path=/i
+standalone.static.do.not.prompt=true
+# path to images must not contain symbolic links
+standalone.static.images=/u01/app/oracle-product/12.2.0.1/dbhome/apex/images
+user.apex.listener.password=${PASS}
+user.apex.restpublic.password=${PASS}
+user.public.password=${PASS}
+user.tablespace.default=APEX
+user.tablespace.temp=TEMP
+sys.user=SYS
+sys.password=${PASS}
+EOF
+}
+
+install(){
+	cd ${ORACLE_HOME}/ords
+	java -jar ords.war configdir ${ORACLE_BASE}/ords/conf
+	java -jar ords.war install \
+		--parameterFile ${ORACLE_BASE}/ords/params/ords_params.properties \
+		--logDir ${ORACLE_BASE}/ords/logs simple
+}
+
+# main
+mkdirs
+set_params
+install
