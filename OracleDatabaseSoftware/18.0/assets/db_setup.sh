27650a07c105313699a4b8e1701e552c201721c5
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2018-10-25 20:25:46 +0200

    use APEX 18.2

--- OracleDatabaseSoftware/18.0/assets/db_setup.sh
+++ OracleDatabaseSoftware/18.0/assets/db_setup.sh
@@ -101,7 +101,6 @@ rm -r -f ${ORACLE_HOME}/apex
 
 # download and extract APEX software
 echo "downloading APEX..."
-wget -q --no-check-certificate ${ORACLE_ASSETS}/apex_18.1_en.zip -O /tmp/apex.zip
 echo "extracting APEX..."
 unzip -o /tmp/apex.zip -d ${ORACLE_HOME} > /dev/null
 chown -R oracle:oinstall ${ORACLE_HOME}/apex
