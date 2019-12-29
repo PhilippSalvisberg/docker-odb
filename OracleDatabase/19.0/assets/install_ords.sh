1b83e55f2462438880a1aac59c65203635dc7fb1
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2019-12-29 12:07:34 +0100

    #2 - create new user ADMIN with all privs to enable all SQL Dev Web features

--- OracleDatabase/19.0/assets/install_ords.sh
+++ OracleDatabase/19.0/assets/install_ords.sh
@@ -58,7 +58,41 @@ install(){
 		--logDir ${ORACLE_BASE}/ords/logs simple
 }
 
+create_user_admin(){
+	sqlplus -s -l sys/${PASS}@${CONNECT_STRING} AS SYSDBA <<EOF
+		BEGIN
+		   EXECUTE IMMEDIATE 'DROP USER admin CASCADE';
+		EXCEPTION
+		   WHEN OTHERS THEN
+		      NULL;
+		END;
+		/
+		CREATE USER admin IDENTIFIED BY ${PASS}
+		   DEFAULT TABLESPACE users
+		   TEMPORARY TABLESPACE temp
+		   QUOTA UNLIMITED ON users;
+		-- all these roles are required to gain full access to all SQL Developer Web components
+		-- see https://docs.oracle.com/en/database/oracle/sql-developer-web/19.4/sdweb/user-management-page.html#GUID-75E439E8-A25B-4E26-821A-2F0F4558AFC4
+		GRANT CONNECT, RESOURCE, PDB_DBA, DBA, ORDS_ADMINISTRATOR_ROLE to admin;
+EOF
+	sqlplus -s -l admin/${PASS}@${CONNECT_STRING} <<EOF
+		BEGIN
+		   ords.drop_rest_for_schema('ADMIN');
+		   ords.enable_schema(
+			   p_enabled             => true,
+			   p_schema              => 'ADMIN',
+			   p_url_mapping_type    => 'BASE_PATH',
+			   p_url_mapping_pattern => 'admin',
+			   p_auto_rest_auth      => false
+		   );
+		   COMMIT;
+		END;
+		/
+EOF
+}
+
 # main
 mkdirs
 set_params
 install
+create_user_admin
